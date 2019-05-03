0a4ccf68126fbfcab0ba6bb59e6be6ae30a3a988
ahmedhajyahmed <hajyahmedboh@yahoo.com>
2019-05-03 03:45:15 +0200

    home+nav+footer

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'jhi-footer',
     templateUrl: './footer.component.html',
-    styleUrls: ['footer.css']
+    styleUrls: []
 })
 export class FooterComponent {}
